b128f8cad88f2f2f173f19cb074c33d8057119ee
carloslme <carloslmescom@gmail.com>
2022-09-30 00:32:17 -0500

    feat: implement black formatter

--- ejemplo/tests/integration/test_integracion.py
+++ ejemplo/tests/integration/test_integracion.py
@@ -13,8 +13,6 @@ from ejemplo.app.funciones import procesar_apellido_paterno
 from ejemplo.app.funciones import procesar_apellido_materno
 
 
-
-
 def concatenar_nombre_completo(nombre, ap, am):
     return nombre + " " + ap + " " + am
 
@@ -26,8 +24,7 @@ def obtener_datos_test_integracion():
     ]
 
 
-@pytest.mark.parametrize("nombre, ap, am, esperado", 
-                        obtener_datos_test_integracion())
+@pytest.mark.parametrize("nombre, ap, am, esperado", obtener_datos_test_integracion())
 def test_divide_parametrize(nombre, ap, am, esperado):
     assert (
         procesar_nombre(nombre)
